dc591f58b65a019cde0da3bcc1c53121ff76a8c0
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-16 18:09:00 -0500

    dashboard tests done

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -14,13 +14,8 @@ class Dashboard extends React.Component {
 
         return (
             <>
-                <Display locked={locked} closed={closed} />
-                <Controls
-                    locked={locked}
-                    closed={closed}
-                    toggleLocked={this.toggleLocked}
-                    toggleClosed={this.toggleClosed}
-                />
+                <Display locked={locked} closed={closed}/>
+                <Controls locked={locked} closed={closed} toggleLocked={this.toggleLocked} toggleClosed={this.toggleClosed}/>
             </>
         );
     }
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+describe('dashboard', () => {
+    test('defaults to unlocked', () => {
+        const {getByText} = render(<Dashboard/>);
+        expect(getByText('Unlocked')).toBeInTheDocument();
+    });
+    test('defaults to open', () => {
+        const {getByText} = render(<Dashboard/>);
+        expect(getByText('Open')).toBeInTheDocument();
+    });
+});
